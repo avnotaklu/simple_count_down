b94d65a2a0635ce0aabd3f5e6517596a1ad28fa8
avnotaklu <sukhmander111101@gmail.com>
2022-02-01 16:47:41 +0530

    fixed some problems from prev commit

--- lib/timer_count_down.dart
+++ lib/timer_count_down.dart
@@ -12,6 +12,10 @@ class Countdown extends StatefulWidget {
   /// Build method for the timer
   final Widget Function(BuildContext, double) build;
 
+  /// time factor of microseconds available in builder
+
+  final int timeFactor;
+
   /// Called when finished
   final Function? onFinished;
 
@@ -28,6 +32,7 @@ class Countdown extends StatefulWidget {
     Key? key,
     required this.duration,
     required this.build,
+    this.timeFactor = 1000000,
     this.interval = const Duration(seconds: 1),
     this.onFinished,
     this.controller,
@@ -41,10 +46,9 @@ class Countdown extends StatefulWidget {
 /// State of timer
 ///
 class _CountdownState extends State<Countdown> {
-
   // Timer
   Timer? _timer;
-  
+
   /// Internal control to indicate if the onFinished method was executed
   bool _onFinishedExecuted = false;
 
@@ -72,8 +76,7 @@ class _CountdownState extends State<Countdown> {
   @override
   void didUpdateWidget(Countdown oldWidget) {
     super.didUpdateWidget(oldWidget);
-    if (oldWidget.duration.inSeconds != widget.duration.inSeconds)
-      _currentMicroSeconds = widget.duration.inMicroseconds;
+    if (oldWidget.duration.inSeconds != widget.duration.inSeconds) _currentMicroSeconds = widget.duration.inMicroseconds;
   }
 
   @override
@@ -89,7 +92,7 @@ class _CountdownState extends State<Countdown> {
   Widget build(BuildContext context) {
     return widget.build(
       context,
-      widget.duration.inMicroseconds,
+      _currentMicroSeconds / widget.timeFactor, 
     );
   }
 
@@ -113,16 +116,16 @@ class _CountdownState extends State<Countdown> {
   /// Then timer restarted
   ///
   void _onTimerReset() {
-    widget.controller?.isCompleted = false;    
+    widget.controller?.isCompleted = false;
     _onFinishedExecuted = false;
 
     setState(() {
       _currentMicroSeconds = widget.duration.inMicroseconds;
     });
-
   }
+
   void _onTimerRestart() {
-    widget.controller?.isCompleted = false;    
+    widget.controller?.isCompleted = false;
     _onFinishedExecuted = false;
 
     setState(() {
@@ -152,13 +155,12 @@ class _CountdownState extends State<Countdown> {
             if (widget.onFinished != null) {
               widget.onFinished!();
               this._onFinishedExecuted = true;
-            }            
+            }
             widget.controller?.isCompleted = true;
           } else {
             this._onFinishedExecuted = false;
             setState(() {
-              _currentMicroSeconds =
-                  _currentMicroSeconds - widget.interval.inMicroseconds;
+              _currentMicroSeconds = _currentMicroSeconds - widget.interval.inMicroseconds;
             });
           }
         },
@@ -167,7 +169,7 @@ class _CountdownState extends State<Countdown> {
       if (widget.onFinished != null) {
         widget.onFinished!();
         this._onFinishedExecuted = true;
-      }      
+      }
       widget.controller?.isCompleted = true;
     }
   }
